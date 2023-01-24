4370d2d85bd8ad06274127a72e540029ef424aad
Jonathan Hawes <jhawes580@gmail.com>
2023-01-24 02:58:51 -0800

    Add font, fix logic, write proper README

--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ import DOMsearch from "./DOMmanip";
 
 load();
 
+// TO-DO: Add a F/C temp format switcher
+// TO-DO: Add a background switcher based on the weather/time of day
+
 const searchSubmit = async (e) => {
   e.preventDefault();
 
   try {
     const input = document.querySelector("input").value;
+
+    // TO-DO: Add DOMsearch().loading() here??
 
     let data = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${input}&APPID=694366407429cf7a8a159e2e0e58bd8c`,
@@ -19,9 +24,14 @@ const searchSubmit = async (e) => {
     then set data equal to it once it returns  */
     data = await data.json();
 
+    if (data.cod === "404") {
+      throw new Error("404 Not Found");
+    }
+
     DOMsearch().success(data);
   } catch (err) {
     console.log(err);
+    DOMsearch().failure();
   }
 };
 
